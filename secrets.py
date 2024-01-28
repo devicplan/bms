46eef0a353ab2fae4ed13ff81ab3cb11c017ebd5
Didi <121518338+devicplan@users.noreply.github.com>
2024-01-28 17:08:23 +0100

    Create secrets.py
    Enter your WLAN name and password here, if required you can also enter your Thinspeak write key (if this is to be used)

---
+++ secrets.py
@@ -0,0 +1,7 @@
+# BMS Controller with Raspberry Pico W 
+# Micropython with Raspberry Pico W
+# 28.01.2024
+
+ssid = 'Your_WLAN_Name'                                                            # Dein WLAN Name
+password = 'Your_WLAN_Password'                                                    # Dein WLAN Passwort
+WRITE_API_KEY = 'Thingspeak_write_api_key'                                         # Dein Schreikennwort fuer Thingspeak
